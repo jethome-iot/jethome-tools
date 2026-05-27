41e9b77aafb48048c82a4e86c38b655b2959dc0a
Nikita Maslo <nikitamalco203@gmail.com>
2026-05-27 15:03:23 +0300

    Delete j300(s7d) files, rename j300y5>j310, add board files for d3(s7 amlogic s905y5 board)

--- convert.sh
+++ convert.sh
@@ -7,12 +7,14 @@ if [ $# -lt 3 ]; then
     echo "	$0 <input> <h1|d1> <type> [compress uboot]"
     echo
     echo "		input		- input image"
-    echo "		h1|d1|d2|j80|j100|j200|cma	- select controller D1 or H1"
+    echo "		h1|d1|d2|j80|j100|j200|j310|cma	- select controller"
     echo "		type		- partition type. supported: haos, armbian"
     echo "		compress	- 'compress' or 'no' output to zip"
-    echo "		uboot		- path to u-boot binary"
+    echo "		uboot		- path to u-boot binary (ignored for j310)"
     exit
 fi
+
+SOC_FAMILY="legacy"
 
 if [[ "$2" == "h1" || "$2" == "j80" ]]; then
   DTS="meson-gxl-s905w-jethome-jethub-j80.dts"
@@ -26,6 +28,10 @@ elif [[ "$2" == "d2" || "$2" == "j200" ]]; then
 elif [[ "$2" == "cma" ]]; then
   DTS="meson-axg-jethome-jethub-j100.dts"
   CNAME="magicbox"
+elif [[ "$2" == "j310" ]]; then
+  DTS="meson-s7-jethub-j310.dts"
+  CNAME="j310"
+  SOC_FAMILY="s7"
 else
   echo "ERROR: unknown controller"
   exit
@@ -35,8 +41,13 @@ if [[ "$3" == "haos" ]]; then
   DTI="partition_haos.dtsi"
   CPART="haos"
 elif [[ "$3" == "armbian" ]]; then
-  DTI="partition_arm.dtsi"
-  CPART="armbian"
+  if [[ "$SOC_FAMILY" == "s7" ]]; then
+    DTI="partition_arm_j310.dtsi"
+    CPART="armbian.s7"
+  else
+    DTI="partition_arm.dtsi"
+    CPART="armbian"
+  fi
 else
   echo "ERROR: unknown partition table"
   exit
@@ -46,7 +57,9 @@ if [[ "$4" == "compress" ]]; then
     COMPRESS=yes
 fi
 
-if [[ -e "$5" ]]; then
+if [[ "$SOC_FAMILY" == "s7" ]]; then
+    UBOOT=""
+elif [[ -e "$5" ]]; then
     UBOOT="$5"
 else
     echo "Error: u-boot binary did not found"
@@ -98,18 +111,42 @@ while read -r line; do
     i=$((i + 1))
 done <<< "$FDISK"
 
+# For s7 armbian: prepend 2x 102 MiB recovery slots before ext4 rootfs so
+# that the on-chip layout matches CONFIG_MBR_ROOTFS_OFFSET_EXTRA (204 MiB) in
+# u-boot. Final part-1.img = recovery_a + recovery_b + ext4.
+if [[ "$3" == "armbian" && "$SOC_FAMILY" == "s7" && -e "bins/$CNAME/recovery.fit" ]]; then
+    SLOT_BYTES=$((102 * 1024 * 1024))
+    echo "Prepending 2x recovery.fit (102 MiB each) to rootfs"
+    cp "bins/$CNAME/recovery.fit" "$TMP/recovery_a.bin"
+    cp "bins/$CNAME/recovery.fit" "$TMP/recovery_b.bin"
+    truncate -s $SLOT_BYTES "$TMP/recovery_a.bin"
+    truncate -s $SLOT_BYTES "$TMP/recovery_b.bin"
+    cat "$TMP/recovery_a.bin" "$TMP/recovery_b.bin" "$TMP/part-1.img" > "$TMP/part-1.img.new"
+    mv "$TMP/part-1.img.new" "$TMP/part-1.img"
+    rm "$TMP/recovery_a.bin" "$TMP/recovery_b.bin"
+fi
+
 cp "bins/$CNAME/platform.conf" "$TMP"
 
 cc -o $TMP/dtbTool dtbtools/dtbTool.c
 $TMP/dtbTool -o "$TMP/_aml_dtb.PARTITION" "$TMP"
 
 cp "bins/image.$CPART.cfg" "$TMP/image.cfg"
-echo cp "$UBOOT" "$TMP/u-boot.bin"
-cp "$UBOOT" "$TMP/u-boot.bin"
-cp "bins/$CNAME/DDR.USB" "$TMP"
-cp "bins/$CNAME/UBOOT.USB" "$TMP"
+if [[ "$SOC_FAMILY" == "s7" ]]; then
+    cp "bins/$CNAME/u-boot.bin" "$TMP/"
+    cp "bins/$CNAME/u-boot.bin.sd.bin" "$TMP/"
+    cp "bins/$CNAME/u-boot.bin.usb" "$TMP/"
+    cp "bins/$CNAME/usb_flow.aml" "$TMP/"
+    PACKER="./tools/aml_image_v2_packer_new.s7"
+else
+    echo cp "$UBOOT" "$TMP/u-boot.bin"
+    cp "$UBOOT" "$TMP/u-boot.bin"
+    cp "bins/$CNAME/DDR.USB" "$TMP"
+    cp "bins/$CNAME/UBOOT.USB" "$TMP"
+    PACKER="./tools/aml_image_v2_packer_new"
+fi
 
-./tools/aml_image_v2_packer_new -r "$TMP/image.cfg" "$TMP" output/$OUTIMG
+$PACKER -r "$TMP/image.cfg" "$TMP" output/$OUTIMG
 
 if [[ "$COMPRESS" == "yes" ]]; then
     cd output
